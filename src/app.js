7b223785b61a13a40a377a781c9d9ed13fb65e05
Yadnyesh <yadnyeshrane1998@gmail.com>
2025-02-22 12:19:15 +0530

    Exploring Routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const app = express();
 app.use("/test", (req, res) => {
     res.send("Hello from server");
 });
+app.get("/user", (re, res) => {
+    res.send({ fname: "Yadnyesh", name: "rane" });
+});
+app.post("/post", (req, res) => {
+    res.send("Dat added succesfully");
+});
 app.listen(3000, () => {
     console.log("Server is Listening");
 });
